89d66b557ecb7f50c94c480f0649b080015ec1b8
Arian Ajdari <ajdari.arian94@gmail.com>
2026-06-28 22:20:08 +0000

    Removed arm

--- triplets/community/arm-poky-linux.cmake
+++
@@ -1,7 +0,0 @@
-set(VCPKG_TARGET_ARCHITECTURE arm)
-set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE static)
-set(VCPKG_CMAKE_SYSTEM_NAME Linux)
-
-# This tells vcpkg to load your Poky toolchain for every port it builds
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../../../tools/toolchains/arm-poky-linux.cmake")
